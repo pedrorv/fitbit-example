911a4864d17514fa9ac2398f511e69a35e71aa01
Pedro Reis <pedroreisv@gmail.com>
2018-07-02 10:33:46 -0300

    Add convertToTimestamp to time utility

--- src/utilities/time.js
+++ src/utilities/time.js
@@ -1,11 +1,16 @@
+const moment = require("moment");
+
 const SECOND = 1000;
 const MINUTE = 60 * SECOND;
 const HOUR = 60 * MINUTE;
 const DAY = 24 * HOUR;
+const convertToTimestamp = (date, hhmmss) =>
+  moment(`${date} ${hhmmss}`, "YYYY-MM-DD HH:mm:ss").format("x");
 
 module.exports = {
   seconds: n => n * SECOND,
   minutes: n => n * MINUTE,
   hours: n => n * HOUR,
-  days: n => n * DAY
+  days: n => n * DAY,
+  convertToTimestamp
 };
