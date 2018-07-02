580f583c7917c867215b2f5622a2fca5ceb057c3
Pedro Reis <pedroreisv@gmail.com>
2018-07-02 10:35:52 -0300

    Remove heartbeat model timestamps + Add storeIntradayActivity

--- src/models/heartbeat.js
+++ src/models/heartbeat.js
@@ -1,13 +1,17 @@
 const { sequelize, Sequelize } = require("../db");
 
-const Heartbeat = sequelize.define("heartbeat", {
-  amount: {
-    type: Sequelize.STRING
+const Heartbeat = sequelize.define(
+  "heartbeat",
+  {
+    amount: {
+      type: Sequelize.STRING
+    },
+    timestamp: {
+      type: Sequelize.INTEGER
+    }
   },
-  timestamp: {
-    type: Sequelize.INTEGER
-  }
-});
+  { timestamps: false }
+);
 
 Heartbeat.sync();
 
--- src/services/heartbeat.js
+++ src/services/heartbeat.js
@@ -1,10 +1,47 @@
 const HeartbeatModel = require("../models/heartbeat");
+const { convertToTimestamp } = require("../utilities/time");
 
 class HeartbeatService {
   static create(amount, timestamp) {
     return HeartbeatModel.findOrCreate({ where: { amount, timestamp } }).spread(
       heartbeat => heartbeat
     );
+  }
+
+  static last() {
+    return HeartbeatModel.findAll({
+      limit: 1,
+      order: [["createdAt", "DESC"]]
+    }).then(heartbeats => heartbeats[0] || null);
+  }
+
+  static async storeIntradayActivity(data) {
+    const transformedData = this.transformResponse(data);
+
+    for (let activity of transformedData) {
+      const { amount, timestamp } = activity;
+      await this.create(amount, timestamp);
+    }
+
+    return true;
+  }
+
+  static transformResponse(intraday) {
+    const activities = intraday["activities-heart"][0];
+    const activitiesIntraday = intraday["activities-heart-intraday"]["dataset"];
+    const date = activities["dateTime"];
+
+    const mapToTimestamp = activity => {
+      const hhmmss = activity.time;
+      const timestamp = convertToTimestamp(date, hhmmss);
+
+      return {
+        timestamp,
+        amount: activity.value
+      };
+    };
+
+    return activitiesIntraday.map(mapToTimestamp);
   }
 }
 
