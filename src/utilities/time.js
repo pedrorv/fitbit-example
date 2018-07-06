1a429d21ea810cf164d2bd317602a29cfa4e1860
Pedro Reis <pedroreisv@gmail.com>
2018-07-05 22:27:32 -0300

    Fix to use unix timestamp

--- src/utilities/time.js
+++ src/utilities/time.js
@@ -5,7 +5,7 @@ const MINUTE = 60 * SECOND;
 const HOUR = 60 * MINUTE;
 const DAY = 24 * HOUR;
 const convertToTimestamp = (date, hhmmss) =>
-  moment(`${date} ${hhmmss}`, "YYYY-MM-DD HH:mm:ss").format("x");
+  moment(`${date} ${hhmmss}`, "YYYY-MM-DD HH:mm:ss").format("X");
 
 module.exports = {
   seconds: n => n * SECOND,
